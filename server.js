24ce8af4189684c8cc805aa75fef0cdaefc6ffc7
mmhsohel <mmhsohel651@gmail.com>
2025-06-19 18:03:43 +0600

    entry file edit from localhost to global host

--- server.js
+++ server.js
@@ -38,4 +38,7 @@ app.use("/", healthRoute);
 
 
 // Start server
-app.listen(port, () => console.log(`🚀 Server running at http://localhost:${port}`));
+app.listen(port, '0.0.0.0', () => {
+  console.log(`🚀 Server running at http://0.0.0.0:${port}`);
+});
+
