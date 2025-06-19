79decaef5609248bb5728e68966014699654d0f1
mmhsohel <mmhsohel651@gmail.com>
2025-06-19 17:55:03 +0600

    further edit the healthe route

--- routes/health.js
+++ routes/health.js
@@ -3,6 +3,7 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/health', (req, res) => {
+  console.log(`[HEALTH CHECK] Ping received at ${new Date().toISOString()}`);
   res.status(200).json({ status: 'ok' });
 });
 
